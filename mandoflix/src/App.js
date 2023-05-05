0de36b63eaa0b9b468eb3e4a4a693e7fcc4ea331
CharlieTheIndieDev <74312830+DoesDevStuff@users.noreply.github.com>
2023-05-05 21:29:12 +0100

    updated Banner

--- mandoflix/src/App.js
+++ mandoflix/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 //import './components/Navbar.css';
-//import './components/Banner.css';
+import './components/Banner.css';
 //import './components/Row.css';
 //import Navbar from './components/Navbar';
-//import Banner from './components/Banner';
+import Banner from './components/Banner';
 //import Row from './components/Row';
 //import RowLarge from './components/RowLarge';
 //import Footer from './components/Footer';
-//import requests from './request';
+import requests from './request';
 
 function App() {
   return (
